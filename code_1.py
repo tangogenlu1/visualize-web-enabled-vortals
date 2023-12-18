a5410cab93c8882203f7d2f435a32a1398e15db0
Людмила <142536689+tangogenlu1@users.noreply.github.com>
2023-12-18 15:58:56 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,26 +1,68 @@
-    return data
+
+    for item in data:
 def main():
         print(f"Random Number: {item}")
-import random
+    return data
 def generate_random_data():
+if __name__ == "__main__":
+
+
     main()
     data = [random.randint(1, 100) for _ in range(10)]
+
+    data = generate_random_data()
+import random        int[] data = new int[10];
+    public static void main(String[] args) {
+}
+    }
+
+            data[i] = (int) (Math.random() * 100) + 1;
+            System.out.println("Random Number: " + item);
+public class RandomDataGenerator {
+        }
+        }
+
+        for (int i = 0; i < 10; i++) {
+        for (int item : data) {
+    return data
+
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+if __name__ == "__main__":
+import random
+    for item in data:
+
+
+def generate_random_data():
+def main():    for item in data:
+    return data
+        print(f"Random Number: {item}")
+
+    main()
 
 
 
 if __name__ == "__main__":
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
     data = generate_random_data()
-
-    for item in data:        for (int item : data) {
-            System.out.println("Random Number: " + item);
-            data[i] = (int) (Math.random() * 100) + 1;
-        }
-        }
-    }
-        int[] data = new int[10];
-public class RandomDataGenerator {
+def main():
+    for (let i = 0; i < 10; i++) {
+main();
+    });
+    const data = generateRandomData();
 }
 
-    public static void main(String[] args) {
+    const data = [];
+        data.push(Math.floor(Math.random() * 100) + 1);
+    }
+    return data;
+        console.log("Random Number: " + item);
+function generateRandomData() {
 
-        for (int i = 0; i < 10; i++) {
+function main() {
+}
+    data.forEach(item => {
